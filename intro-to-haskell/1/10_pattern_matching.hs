5f5e1f975073df4c479cdbd84822369cf273f2f8
CraftyDragon678 <craftydragon678@gmail.com>
2020-08-25 06:56:31 +0900

    define many things..?

---
+++ intro-to-haskell/1/10_pattern_matching.hs
@@ -0,0 +1,24 @@
+not :: Bool -> Bool
+not False = True
+not True = False
+
+-- switch~case없이 나타낼 수 있음
+
+(&&) :: Bool -> Bool -> Bool
+
+True && True = True
+True && False = False
+False && True = False
+False && False = False
+
+-- same as
+
+True && True = True -- 오른쪽이 True인지 평가(evaluate)함
+_ && _ = False  -- wild card pattern
+
+-- same as
+
+True && b = b -- 오른쪽을 평가하지 않음
+False && _ = False
+
+-- b && b = b -- error
