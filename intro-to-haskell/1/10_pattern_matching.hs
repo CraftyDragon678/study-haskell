f635f613dd7761dcc26417f51134f7468a0beea6
CraftyDragon678 <craftydragon678@gmail.com>
2020-08-27 08:58:27 +0900

    pattern matching or

--- intro-to-haskell/1/10_pattern_matching.hs
+++ intro-to-haskell/1/10_pattern_matching.hs
@@ -4,21 +4,24 @@ not True = False
 
 -- switch~case없이 나타낼 수 있음
 
-(&&) :: Bool -> Bool -> Bool
+(&&&) :: Bool -> Bool -> Bool
 
-True && True = True
-True && False = False
-False && True = False
-False && False = False
-
--- same as
-
-True && True = True -- 오른쪽이 True인지 평가(evaluate)함
-_ && _ = False  -- wild card pattern
+True &&& True = True
+True &&& False = False
+False &&& True = False
+False &&& False = False
 
 -- same as
 
-True && b = b -- 오른쪽을 평가하지 않음
-False && _ = False
+True &&& True = True -- 오른쪽이 True인지 평가(evaluate)함
+_ &&& _ = False  -- wild card pattern
+
+-- same as
+
+True &&& b = b -- 오른쪽을 평가하지 않음
+False &&& _ = False
 
 -- b && b = b -- error
+
+False ||| False = False
+_ ||| _ = True
