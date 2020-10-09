82bfc40292922adb271ed5c6aba5a72eba9f4b8f
CraftyDragon678 <craftydragon678@gmail.com>
2020-10-09 15:29:40 +0900

    church_numerals comments

--- intro-to-haskell/3/14_church_numerals.hs
+++ intro-to-haskell/3/14_church_numerals.hs
@@ -1,3 +1,9 @@
+-- 원래처럼 함수를 사용해서 정의를 한다면,,,
+zero f x = x
+one f x = f x
+two f x = (f . f) x
+
+-- 람다를 활용한 아래와 같음
 zero = \s z -> z
 one = \s z -> s z
 two = \s z -> s (s z)
@@ -8,6 +14,12 @@ two = \s z -> s (s z)
 c2i x = x (+1) 0
 c2s x = x ('*':) ""
 
+-- c2i zero = 0
+-- c2i one = 1
+-- c2i two = 2
+-- c2s zero = ""
+-- c2s one = "*"
+-- c2s two = "**"
 
 -- 증명 part~~~ 문법과는 맞지 않아요~~
 -- x 와 y 함수를 합치고 싶다,,
@@ -18,13 +30,25 @@ y' = c2i y
 -- 어쨌는 위 두개는 수
 -- 두개를 더할 거임 ㅇㅇ
 
--- 증명 타임~
+-- 이런 꼴이라고 가정
+x' + y' = c2i (add x y)
+
+two = \s -> s . s
+two = \s z -> (s . s) z
+
+c2i x = x (+1) 0
+-- 합 증명 타임~
 x' + y'
 = c2i x + c2i y
 = x (+1) 0 + c2i y
 = x (+1) (c2i y)
 = x (+1) (y (+1) 0)
-= (\s z -> x s (y s z)) (+1) 0
+= (\s z -> x s (y s z)) (+1) 0  -- 이 부분
+-- z: init value
+-- s: successor function -> 공통인 친구니깐 얘를 찢어서 같이 둠
+-- y s z의 값을 x s 의 인자로 전달
+-- y s z에서 s z는 (+1) 와 0
+
 
 -- add 함수의 역할은 아래와 같습니다
 x' + y' = (add x y) (+1) 0
@@ -32,16 +56,24 @@ x' + y' = (add x y) (+1) 0
 
 add x y = (\s z -> x s (y s z))
 
-c2i (add one two)
+c2i $ add one two
 
 
 -- 곱셈,,
-two = \s -> s . s
-three = \s -> s . s . s
+two = \s -> s.s
+three = \s -> s.s.s
+four = \s -> s.s.s.s
+five = \s -> s.s.s.s.s
 
 mul = \s z -> s (y s) z
 c2i (mul two five)
 
+
+-- 정리
+-- 더하는 연산은 초기값이 이전 함수의 결과가 되고,
+-- 곱하는 연산은 successor 함수가 이전 함수에 successor를 넣은 함수가 된다
+
+-- identity
 id :: a -> a
 id = \x -> x
 
