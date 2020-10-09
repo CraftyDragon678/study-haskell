cb9723f9cafa77aa30e6f588429fb2a0e3f4d2c1
CraftyDragon678 <craftydragon678@gmail.com>
2020-10-09 18:40:51 +0900

    monad again: make instances

--- intro-to-haskell/4/08_monad_again.hs
+++ intro-to-haskell/4/08_monad_again.hs
@@ -1,13 +1,22 @@
+-- https://www.seas.upenn.edu/~cis552/15fa/lectures/Parsers.html
 import Control.Monad
 
+-- a는 타입매개변수 
 newtype Parser a = P (String -> [(a, String)])
+
+instance Functor Parser where
+  fmap = liftM
+
+instance Applicative Parser where
+  pure = return
+  (<*>) = ap
 
 instance Monad Parser where
   return v = P $ \inp -> [(v, inp)]
   p >>= f = P $ \inp -> case parse p inp of
     [] -> []
     [(v, out)] -> parse (f v) out
-
+  
 item :: Parser Char
 item = P $ \inp -> case inp of
   [] -> []
