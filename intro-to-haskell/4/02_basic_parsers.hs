8e92682bb3624a499589ad5db768c08a4b049497
CraftyDragon678 <craftydragon678@gmail.com>
2020-10-09 16:12:51 +0900

    basic parsers add comment

--- intro-to-haskell/4/02_basic_parsers.hs
+++ intro-to-haskell/4/02_basic_parsers.hs
@@ -1,15 +1,15 @@
 type Parser a = String -> [(a, String)]
 
-item :: Parser Char
+item :: Parser Char   -- 문자열의 첫 부분을 파싱하는 파서
 item = \xs -> case xs of
-  [] -> []
+  [] -> []            -- 실패했당,,, 문자열이 아무것도 없어서
   (x:xs) -> [(x, xs)]
 
--- 항상 []만
+-- 항상 []만 => 항상 실패하는 파서
 failure :: Parser a
-failure = \xs -> []
+failure = \_ -> []
 
-returnC :: a -> Parser a
+returnC :: a -> Parser a  -- 항상 성공하는 파서
 returnC v = \xs -> [(v, xs)]
 
 -- 두개의 파서를 합치는 함수 (+++)
