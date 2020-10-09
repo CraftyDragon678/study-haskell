a2a705e9dd302222b94986d949038c615770cb5e
CraftyDragon678 <craftydragon678@gmail.com>
2020-10-09 18:09:15 +0900

    control,,,

---
+++ wikidocs/2. basic/control.hs
@@ -0,0 +1,11 @@
+main =
+ do x <- getX
+    putStrLn x
+
+getX =
+ do return "My Shangri-La"
+    return "beneath"
+    return "the summer moon"
+    return "I will"
+    return "return"
+    return "again"
