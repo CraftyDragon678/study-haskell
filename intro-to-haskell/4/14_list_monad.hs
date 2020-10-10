a6a66d83986e8a6dec1a22fd2830994711708988
CraftyDragon678 <craftydragon678@gmail.com>
2020-10-10 10:39:54 +0900

    add comment in list monad

--- intro-to-haskell/4/14_list_monad.hs
+++ intro-to-haskell/4/14_list_monad.hs
@@ -2,6 +2,8 @@
 --   return x = [x]
 --   xs >>= f = concat (map f xs)
 
+-- xs >>= f = [y | x <- xs, y <- f x]
+
 pairs :: [a] -> [b] -> [(a, b)]
 -- pairs xs ys = xs >>= \x -> ys >>= \y -> return (x, y)
 pairs xs ys = do x <- xs
