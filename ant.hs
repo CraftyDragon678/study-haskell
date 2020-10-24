35b41987b61813bf9f516797496a4832ef879a3b
CraftyDragon678 <craftydragon678@gmail.com>
2020-10-24 19:55:35 +0900

    ant more

--- ant.hs
+++ ant.hs
@@ -3,6 +3,13 @@ import Data.List
 f :: [[Int]]
 f = iterate (concatMap (\x -> [head x, length x]) . group) [1]
 
+f2 :: [Int] -> [Int]
+f2 xs = concatMap (\(x,y) -> [x, y]) [(head x, length x) | x <- group xs]
+
+f3 :: [[Int]]
+f3 = iterate (\xs -> concatMap (\(x,y) -> [x, y]) [(head x, length x) | x <- group xs]) [1]
+
+
 main :: IO ()
 main = do
   putStr "> "
